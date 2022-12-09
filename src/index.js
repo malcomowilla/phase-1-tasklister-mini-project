9426c2ea83e87a111175536d9cc427f7d4386272
malcomowilla <owillamalcom@gmail.com>
2022-12-09 09:31:45 +0300

    completed assighnment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  // your code herez
+
+const form = document.getElementById('create-task-form');
+form = addEventListener('submit',dataSubmit)
+
+})
+function dataSubmit (event){
+    event.preventDefault()
+     // console.log("hfhfdj")
+
+     listOfTasks= document.getElementById('tasks')
+     const tasks = document.createElement('li')
+     tasks.textContent =event.target.description.value;
+     listOfTasks.append(tasks);
+
+    const deleteButton = document.createElement('button')
+  deleteButton.textContent = 'x'
+  deleteButton.addEventListener('click', () => {
+    //console.log ('xxx')
+  task.remove()
+  });
+  task.append(deleteButton);
+
+  event.target.reset();
+}
+
+
+
+     
+
+
+
+
+
+
